876b5958d32047742adc21bba36cd0ab9bdbba4c
Teodor Dobromir <teodordobromir@gmail.com>
2025-07-23 19:56:24 +0300

    Small import change in css

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-green-100">
-        <h1 className="text-3xl font-bold text-green-800">
-          Tailwind merge perfect! ✅
-        </h1>
-      </div>
     </>
   );
 }
